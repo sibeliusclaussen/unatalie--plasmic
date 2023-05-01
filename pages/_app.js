3dcf8aea0081a5143535eb1eca803e5a72c43e36
Sibelius Claussen <sibeliusclaussen@gmail.com>
2023-05-01 11:18:02 -0300

    Script Addition

--- pages/_app.js
+++ pages/_app.js
@@ -2,23 +2,25 @@
 import '../styles/globals.css'
 import { PlasmicRootProvider } from "@plasmicapp/react-web";
 import Head from "next/head";
+import Script from 'next/script';
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      <Head>
-        <Script type="text/javascript" src="https://cdn.weglot.com/weglot.min.js"></Script>
-        <Script>
-            Weglot.initialize({
-              api_key : 'wg_beb8dc81dc72eaed78f9f9b5b7f469ac9',
-            });
-        </Script> 
-      </Head>
+      <Script
+        src="https://cdn.weglot.com/weglot.min.js"
+        onLoad={() => {
+          Weglot.initialize({
+            api_key : 'wg_beb8dc81dc72eaed78f9f9b5b7f469ac9'
+        })}}
+        onError={() => setHasError(true)}
+        onReady={() => setIsReady(true)}
+      />
       <PlasmicRootProvider Head={Head}>
         <Component {...pageProps} />
-      </PlasmicRootProvider>  
+      </PlasmicRootProvider>    
     </>
-    
+
   );
 }
 
